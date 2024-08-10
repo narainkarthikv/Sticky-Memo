9407aace714fe13896fd377d50a25592049f29cb
AndresCasasola <andres97dominguez@gmail.com>
2024-08-10 22:46:44 +0200

    feat: add persistence to items state

--- src/utils/state.js
+++ src/utils/state.js
@@ -1,6 +1,10 @@
 import { atom } from 'recoil';
+import { recoilPersist } from 'recoil-persist';
+
+const { persistAtom } = recoilPersist()
 
 export const itemsState = atom({
   key: 'itemsState',
   default: [],
+  effects_UNSTABLE: [persistAtom],
 });
