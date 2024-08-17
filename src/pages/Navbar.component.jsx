764c7a104920601a87f16f024484738a19b85fe1
divyansh <divyanshverma689@gmail.com>
2024-08-17 18:42:55 +0530

    toggle light and dark theme functionalty added

--- src/pages/Navbar.component.jsx
+++ src/pages/Navbar.component.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import { Link } from 'react-router-dom';
-import { FaUserAlt, FaHome, FaTable } from "react-icons/fa";
+import { FaUserAlt, FaHome, FaChalkboard, FaTable } from "react-icons/fa";
 import '../styles/Navbar.component.css';
+import DarkMode from '../components/DarkMode/DarkMode'
+
 
 const Navbar = () => {
     return (
@@ -11,16 +13,22 @@ const Navbar = () => {
                 <Link to="/" className="nav-items">
                     <FaHome /><span className="nav-hide">&nbsp;Home</span>
                 </Link>
+                <Link to="/Boards" className="nav-items">
+                    <FaChalkboard /><span className="nav-hide">&nbsp;Boards</span>
+                </Link>
                 <Link to="/Tables" className="nav-items">
                     <FaTable /><span className="nav-hide">&nbsp;Tables</span>
                 </Link>
             </div>
+            <div className="btn-pro">
+            <DarkMode/>
             <div className="nav-profile">
                 <FaUserAlt className="profile-logo" />
                 <div className="dropdown">
                     <Link to="/editUser" className="dropdown-items"><span>Edit Profile</span></Link>
                     <a href="/logout" className="dropdown-items"><span>Logout</span></a>
                 </div>
+            </div>
             </div>
         </div>
     );
