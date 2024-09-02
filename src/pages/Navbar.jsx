c772589daa08ae4afe33394a708279b706ccca1b
narainkarthikv <narainkarthik812@gmail.com>
2024-09-02 19:53:59 +0530

    fix: updated navbar

--- src/pages/Navbar.jsx
+++ src/pages/Navbar.jsx
@@ -3,27 +3,34 @@ import { Link } from 'react-router-dom';
 import { FaUserAlt, FaChalkboard, FaTable } from "react-icons/fa";
 import '../styles/Navbar.css';
 
-const Navbar = () => {
-    return (
-        <div className="navbar">
-            <Link to="/" className="nav-brand">Sticky Memo</Link>
-            <div className="navbar-center-btns">
-                <Link to="/Boards" className="nav-items">
-                    <FaChalkboard /><span className="nav-hide">&nbsp;Boards</span>
-                </Link>
-                <Link to="/Tables" className="nav-items">
-                    <FaTable /><span className="nav-hide">&nbsp;Tables</span>
-                </Link>
-            </div>
-            <div className="nav-profile">
-                <FaUserAlt className="profile-logo" />
-                <div className="dropdown">
-                    <Link to="/editUser" className="dropdown-items"><span>Edit Profile</span></Link>
-                    <a href="/logout" className="dropdown-items"><span>Logout</span></a>
-                </div>
-            </div>
+const Navbar = () => (
+    <nav className="navbar">
+        
+        <Link to="/" className="nav-brand">Sticky Memo</Link>
+        
+        <div className="navbar-center-btns">
+            <NavItem to="/Boards" icon={<FaChalkboard />} label="Boards" />
+            <NavItem to="/Tables" icon={<FaTable />} label="Tables" />
         </div>
-    );
-};
+        
+        <ProfileDropdown />
+    </nav>
+);
+
+const NavItem = ({ to, icon, label }) => (
+    <Link to={to} className="nav-items">
+        {icon}<span className="nav-hide">&nbsp;{label}</span>
+    </Link>
+);
+
+const ProfileDropdown = () => (
+    <div className="nav-profile">
+        <FaUserAlt className="profile-logo" />
+        <div className="dropdown">
+            <Link to="/editUser" className="dropdown-items edit"><span>Edit Profile</span></Link>
+            <a href="/logout" className="dropdown-items logout"><span>Logout</span></a>
+        </div>
+    </div>
+);
 
 export default Navbar;
